49f38d0d5de335fdb1890126b05ce9fb00d23a2f
jeongjiwon <jungkt0x@naver.com>
2023-02-26 22:55:31 +0900

    first commit

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,40 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+
+
+}
